95213d6a335fd6a8e9de91ce918823a2a52150c8
Denissined01 <119708367+Denissined01@users.noreply.github.com>
2023-06-13 15:22:03 +0200

    6

---
+++ script.js
@@ -0,0 +1,40 @@
+// JavaScript-Datei: script.js
+
+var cartItems = [];
+var cartTotal = 0;
+
+function addToCart() {
+    var quantity = document.getElementById('quantity').value;
+
+    var product = {
+        name: 'Produktname',
+        price: 10,
+        quantity: parseInt(quantity)
+    };
+
+    cartItems.push(product);
+    updateCart();
+}
+
+function updateCart() {
+    var cartList = document.querySelector('.cart__items');
+    var cartTotalElement = document.querySelector('.cart__total');
+
+    cartList.innerHTML = '';
+
+    cartTotal = 0;
+
+    cartItems.forEach(function (product) {
+        var li = document.createElement('li');
+        li.textContent = product.name + ' - ' + product.price + ' x ' + product.quantity;
+        cartList.appendChild(li);
+
+        cartTotal += product.price * product.quantity;
+    });
+
+    cartTotalElement.textContent = cartTotal;
+}
+
+function checkout() {
+    // Hier können Sie weitere Logik implementieren, um den Checkout-Prozess durchzuführen
+}
